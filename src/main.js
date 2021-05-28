e3f7cc0c1892b4a776ba11bc761e6d14238de1e4
Guyue <2021565598@qq.com>
2021-05-28 21:27:57 +0800

    移动端REM适配

--- src/main.js
+++ src/main.js
@@ -3,8 +3,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import './styles/index.less'
+
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+// 自动设置 REM 基准值
+import 'amfe-flexible'
+
+// 全局注册 Vant 中的组件
+Vue.use(Vant)
 Vue.config.productionTip = false
 
+// 创建 Vue 根实例，将 router、store 配置到根实例中
+// 将 App 根组件渲染到 #app 节点
 new Vue({
   router,
   store,
